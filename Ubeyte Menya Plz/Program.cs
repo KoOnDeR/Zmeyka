f12f93c8e01c9084fd93397840c059accff4937b
Ko_OnDeR <fikeeee@mail.ru>
2019-10-26 22:45:38 +0300

    Initial commit

---
+++ Ubeyte Menya Plz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubeyte_Menya_Plz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
