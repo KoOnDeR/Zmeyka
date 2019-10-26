5b7b92ccf711007cb2c2296bee4222e45c599017
Ko_OnDeR <fikeeee@mail.ru>
2019-10-27 02:49:19 +0300

    added Map drawing Нарисовал карту =-)

--- Ubeyte Menya Plz/Program.cs
+++ Ubeyte Menya Plz/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ubeyte_Menya_Plz
 {
-    class Program
+
+    class Game
     {
-        static void Main(string[] args)
+        public void Map()
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+             void Draw()
+            {
+                int high = 20;
+                int ln = 70;
+                string[,] Map = new string[high, ln];
+                for (int i = 0; i < high; i++)
+                    Map[i, 0] = "#";
+                for (int i = 1; i < high - 1; i++)
+                    for (int j = 1; j < ln - 1; j++)
+                        Map[i, j] = " ";
+                for (int i = 1; i < ln - 1; i++)
+                    Map[0, i] = "#";
+                for (int i = 1; i < ln - 1; i++)
+                    Map[high - 1, i] = "#";
+                for (int i = 0; i < high; i++)
+                    Map[i, ln - 1] = "#";
+                for (int i = 0; i < high; i++)
+                {
+                    for (int j = 0; j < ln; j++)
+                        Console.Write(Map[i, j]);
+                    Console.WriteLine();
+                }
+            }
+            Draw();
+
+
+
+
         }
     }
+    static void Main()
+    {
+        Draw();
+    }
+    
 }
