ad44a0f01581defd30f33d3a8467a2508fd7d0a6
Ko_OnDeR <fikeeee@mail.ru>
2019-10-30 16:58:19 +0300

    Изменена рисовка карты Карта нарисованна не из массива типа string,как было ранее, а из массива типа char.

---
+++ Ubeyte Menya Plz/Map.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubeyte_Menya_Plz
+{
+    class Map
+    {
+        public static int ln = 70;
+        public static int high = 20;
+        public char[,] map = new char[high, ln];
+        public void MapDraw()
+        {
+
+            for (int i = 0; i < high; i++)
+                map[i, 0] = '#';
+            for (int i = 1; i < high; i++)
+                for (int j = 1; j < ln; j++)
+                    map[i, j] = ' ';
+            for (int i = 1; i < ln ; i++)
+                map[0, i] = '#';
+            for (int i = 1; i < ln; i++)
+                map[high - 1, i] = '#';
+            for (int i = 0; i < high; i++)
+                map[i, ln - 1] = '#';
+
+
+            for (int i = 0; i < high; i++)
+            {
+                for (int j = 0; j < ln; j++)
+                    Console.Write(map[i, j]);
+                Console.WriteLine();
+            }
+          
+        }
+        
+    }
+}
